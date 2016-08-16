f63965679f5a0e8a4d357c3ecefa86d9f50af3a0
Jordomav <jordomav@gmail.com>
2016-08-16 00:37:16 -0700

    added more detailed comments

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int left = 10;
 int right = 11;
 
 void setup () {
+//    Begin watching for keyboard inputs
     Keyboard.begin();
 
 //    Button pin modes
@@ -37,57 +38,57 @@ void loop () {
     int yellowPress = digitalRead(yellowBtn);
     int greenPress = digitalRead(greenBtn);
 
-//    digital read variables for joystick
+// digital read variables for joystick
     int joystickUp = digitalRead(forward);
     int joystickDown = digitalRead(backward);
     int joystickLeft = digitalRead(left);
     int joystickRight = digitalRead(right);
 
     if (redPress == 0) {
-        Keyboard.press(87);
+        Keyboard.press(87); // W key pressed
     } else {
-        Keyboard.release(87);
+        Keyboard.release(87); // W key released
     }
 
     if (bluePress == 0) {
-        Keyboard.press(65);
+        Keyboard.press(65); // A key pressed
     } else {
-        Keyboard.release(65);
+        Keyboard.release(65); // A key released
     }
 
     if (yellowPress == 0) {
-        Keyboard.press(83);
+        Keyboard.press(83); // S key pressed
     } else {
-        Keyboard.release(83);
+        Keyboard.release(83); // S key released
     }
 
     if (greenPress == 0) {
-        Keyboard.press(68);
+        Keyboard.press(68); // D key pressed
     } else {
-        Keyboard.release(68);
+        Keyboard.release(68); // D key released
     }
 
     if (joystickUp == 0) {
-        Keyboard.press(38);
+        Keyboard.press(38); // UP key pressed
     } else {
-        Keyboard.release(38);
+        Keyboard.release(38); // UP key released
     }
 
     if (joystickDown == 0) {
-        Keyboard.press(40);
+        Keyboard.press(40); // DOWN key pressed
     } else {
-        Keyboard.release(40);
+        Keyboard.release(40); // Down key released
     }
 
     if (joystickLeft == 0) {
-        Keyboard.press(37);
+        Keyboard.press(37); // LEFT key pressed
     } else {
-        Keyboard.release(37);
+        Keyboard.release(37); // LEFT key released
     }
 
     if (joystickRight == 0) {
-        Keyboard.press(39);
+        Keyboard.press(39); // RIGHT key pressed
     } else {
-        Keyboard.release(39);
+        Keyboard.release(39); // RIGHT key released
     }
 }
