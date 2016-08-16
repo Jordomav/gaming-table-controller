4e9f18a78e72dc86e405ecf70858a17c66a1ade4
Jordomav <jordomav@gmail.com>
2016-08-15 18:21:08 -0700

    added digital read variables to loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 // Button variables
 int redBtn = 4;
 int blueBtn = 5;
-int yelwBtn = 6;
+int yellowBtn = 6;
 int greenBtn = 7;
 
 // Joystick variables
 int forward = 8;
-int backawrd = 9;
+int backward = 9;
 int left = 10;
 int right = 11;
 
@@ -19,16 +19,27 @@ void setup () {
 //    Button pin modes
     pinMode(redBtn, INPUT_PULLUP);
     pinMode(blueBtn, INPUT_PULLUP);
-    pinMode(yelwBtn, INPUT_PULLUP);
+    pinMode(yellowBtn, INPUT_PULLUP);
     pinMode(greenBtn, INPUT_PULLUP);
 
 //    Joystick pin modes
     pinMode(forward, INPUT_PULLUP);
-    pinMode(backawrd, INPUT_PULLUP);
+    pinMode(backward, INPUT_PULLUP);
     pinMode(left, INPUT_PULLUP);
     pinMode(right, INPUT_PULLUP);
 }
 
 void loop () {
+// digital read variables for buttons
+    int redPress = digitalRead(redBtn);
+    int bluePress = digitalRead(blueBtn);
+    int yellowPress = digitalRead(yellowBtn);
+    int greenPress = digitalRead(greenBtn);
+
+//    digital read variables for joystick
+    int joystickUp = digitalRead(forward);
+    int joystickDown = digitalRead(backward);
+    int joystickLeft = digitalRead(left);
+    int joystickRight = digitalRead(right);
 
 }
